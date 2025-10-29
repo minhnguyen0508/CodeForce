51178c54f7150d8d12806e073fb77f35185e7cbd
minhnguyen0508 <minhtnnguyen0508@gmail.com>
2025-10-29 13:58:48 -0700

    change it to fully work project, put work in a directory

--- Codeforce Practice Problem A/Codeforce Practice Problem A/Codeforce Practice Problem A.cpp
+++ Codeforce Practice Problem A/Codeforce Practice Problem A/Codeforce Practice Problem A.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 int solution(int input) {
     int size = input * 3;
-    int *problem = new int[size];
+    int* problem = new int[size];
     for (int i = 0; i < size; i++) {
         std::cin >> problem[i];
     }
